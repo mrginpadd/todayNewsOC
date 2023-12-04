ffde8922844015b10e1b3a47a6e463e527def45d
徐世豪 <1755883481@qq.com>
2023-12-04 16:54:48 +0800

    [新功能] 推荐页scrollView

--- today_news_oc/biz_modules/star_module/view_controllers/start_view_controller.m
+++ today_news_oc/biz_modules/star_module/view_controllers/start_view_controller.m
@@ -8,16 +8,35 @@
 #import <Foundation/Foundation.h>
 #import "star_view_controller.h"
 
-@interface StarViewController ()
-
+@interface StarViewController ()<UIScrollViewDelegate>
+-(void)buildViews;
 @end
 
 @implementation StarViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self buildViews];
 }
 
-
+-(void)buildViews {
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    scrollView.contentSize = CGSizeMake(self.view.frame.size.width * 5, self.view.frame.size.height);
+    scrollView.showsHorizontalScrollIndicator = NO;
+    scrollView.pagingEnabled = YES;
+    scrollView.delegate = self;
+    
+    
+    NSArray *colorArray = @[[UIColor redColor], [UIColor yellowColor], [UIColor greenColor], [UIColor blueColor], [UIColor blackColor]];
+    
+    for (int i=0; i<5; i++) {
+        [scrollView addSubview:({
+            UIView *view = [[UIView alloc] initWithFrame: CGRectMake(i*scrollView.bounds.size.width, 0, scrollView.bounds.size.width, scrollView.bounds.size.height)];
+            view.backgroundColor = colorArray[i];
+            view;
+        })];
+    }
+    
+    [self.view addSubview:scrollView];
+}
 @end
--- today_news_oc/biz_modules/video_module/view_controllers/video_view_controller.m
+++ today_news_oc/biz_modules/video_module/view_controllers/video_view_controller.m
@@ -8,8 +8,9 @@
 #import <Foundation/Foundation.h>
 #import "video_view_controller.h"
 
-@interface VideoViewController ()
+@interface VideoViewController ()<UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource>
 
+- (void)buildViews;
 @end
 
 @implementation VideoViewController
@@ -17,7 +18,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self buildViews];
+}
+
+- (void)buildViews {
+    UICollectionViewFlowLayout *flowLayOut = [[UICollectionViewFlowLayout alloc] init];
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:flowLayOut];
+    
+    flowLayOut.minimumLineSpacing = 10; //行间距
+    flowLayOut.minimumInteritemSpacing = 20; //单元格列最小间距
+    double itemW = (self.view.frame.size.width - 20) / 2;
+    double itemH = 300;
+    flowLayOut.itemSize = CGSizeMake(itemW, itemH);
+    
+    collectionView.delegate = self;
+    collectionView.dataSource = self;
+    
+    [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"UICollectionViewCell"];
+    
+    [self.view addSubview:collectionView];
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 20;
+}
+
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"UICollectionViewCell" forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor redColor];
+    return cell;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 20;
 }
 
 
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.item % 3 == 0) {
+        return CGSizeMake(self.view.frame.size.width, 80);
+    } else {
+        return CGSizeMake((self.view.frame.size.width - 20) / 2, 300);
+    }
+}
 @end
