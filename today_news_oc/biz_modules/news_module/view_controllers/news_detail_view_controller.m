0cd4ff504f4debbeb31d2eb7588e4d3ce72a6be2
徐世豪 <1755883481@qq.com>
2023-12-05 23:38:20 +0800

    [新功能] kvo使用，webView加载进度条

--- today_news_oc/biz_modules/news_module/view_controllers/news_detail_view_controller.m
+++ today_news_oc/biz_modules/news_module/view_controllers/news_detail_view_controller.m
@@ -10,7 +10,7 @@
 @interface NewsDetailViewController ()<WKNavigationDelegate>
 
 @property(nonatomic, strong, readwrite) WKWebView *webView;
-
+@property(nonatomic, strong, readwrite) UIProgressView *progressView; //加载进度条
 @end
 
 @implementation NewsDetailViewController
@@ -28,9 +28,31 @@
         self.webView;
     })];
     
-    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://time.geekbang.org/resource?pt=4&utm_campaign=geektime_search&utm_content=geektime_search&utm_medium=geektime_search&utm_source=geektime_search&utm_term=geektime_search"]]];
+    
+    [self.view addSubview:({
+        self.progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 90, self.view.frame.size.width, 20)];
+        
+        self.progressView;
+    })];
+    
+    [self layoutViews];
+
 }
 
+- (void)layoutViews {
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://time.geekbang.org/resource?pt=4&utm_campaign=geektime_search&utm_content=geektime_search&utm_medium=geektime_search&utm_source=geektime_search&utm_term=geektime_search"]]];
+   
+    // 监听self.webView 的estimatedProgress变化
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+    
+}
+
+- (void)dealloc {
+    // 移除监听
+    [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
+}
+
+#pragma WKWebView代理方法
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
     NSLog(@"decidePolicyForNavigationAction");
     decisionHandler(WKNavigationActionPolicyAllow);
@@ -39,15 +61,13 @@
     NSLog(@"didFinishNavigation");
 }
 
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+// 属性监听的回调
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    NSLog(@"%@", change[@"new"]);
+    self.progressView.progress = self.webView.estimatedProgress;
+    if (self.progressView.progress == 1) {
+        [self.progressView removeFromSuperview];
+    }
 }
-*/
 
 @end
