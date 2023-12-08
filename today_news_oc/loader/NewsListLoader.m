8bd1080b0b06fa92bf29cbb1206632bab67d8ced
徐世豪 <1755883481@qq.com>
2023-12-08 23:51:00 +0800

    [新功能] 引入afnetworking，以及使用原本的网络请求解析二进制流数据

--- today_news_oc/loader/NewsListLoader.m
+++ today_news_oc/loader/NewsListLoader.m
@@ -6,7 +6,7 @@
 //
 
 #import "NewsListLoader.h"
-
+#import <AFNetworking/AFNetworking.h>
 @interface NewsListLoader()
 @property(nonatomic, strong, readonly) NSString *apiKey;
 @end
@@ -18,16 +18,27 @@
     NSString *urlString = @"http://v.juhe.cn/toutiao/index?type=top&key=78e827441812527922718592f358d7bf";
     
     NSURL *listUrl = [NSURL URLWithString:urlString];
+
     
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:listUrl];
-    
+
     NSURLSession *session = [NSURLSession sharedSession];
-    
+
     NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSLog(@"%@", response);
+        NSError *jsonError = nil;
+        id jsonObj = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+//        NSLog(@"%@", response);
     }];
-    
+
     [dataTask resume];
+    
+    
+    // AFNetworking自动帮我们解析了结果，而上面的是二进制流
+//    [[AFHTTPSessionManager manager] GET:urlString parameters:nil headers:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//            NSLog(@"");
+//        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//            NSLog(@"");
+//        }];
     
 }
 
